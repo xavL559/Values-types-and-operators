08b99fe5ae1e58ca5f3cb34cee15fd42c0edc9a7
Xavier Leftridge <xavierleftridge7@gmail.com>
2022-10-31 20:30:27 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+// For each of the 10 tasks below, write a console.log(...) statement that satisfies the
+// requirement. Write your code to accomplish the programming task beneath each comment.
+
+// 1. Write code to output the following string to the console:
+// "Hello World!"
+console.log('Hello World!');
+
+// 2. Write code to concatenate three strings and output the result to the console:
+// "Hello", " ", "World!"
+const str = 'Hello' + ', ' + 'World' ;
+
+// 3. Write code to output "Hello" on one line and "World!" on the next line to the console.
+// *Note: You should only have one console.log() statement - how do you add a new line in
+// a string?...
+console.log('Hello\nWorld!');
+
+// 4. Write code to output the result of the arithmetic expression to the console:
+// 3 * 8 + 2;
+let x = 3 * 8 + 2;
+console.log(x);
+
+// 5. Write code to output the result of the comparison:
+// 5 is less than 10.
+console.log(5<10);
+
+// 6. Write code to output the result of the comparison:
+// 10 is greater than 5.
+console.log(5<10);
+
+// 7. Write code to output the result of the comparison:
+// "Apples" is NOT equal to "Oranges"
+console.log("apples" != "oranges");
+
+// 8. Write code to output the result of the comparison:
+// 10 * 10 is greater than 50 AND 5 * 5 is less than 50
+
+console.log(10*10>50&&5*5<50);
+
+// 9. Write code to output the result of the comparison:
+// 1 + 2 is greater than 5 OR 1 * 10 is greater than 5 
+console.log(1+2>5||1*10>5);
+
+// 10. Write code that demonstrates an automatic type conversion as shown in Chapter 01.
+
+console.log("10" - 5)
